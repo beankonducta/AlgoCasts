397f899627f399dd9f62a434401699e6e1cfefee
patrick <patrickxandrews@gmail.com>
2021-01-09 17:20:55 -0700

    fib finished

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     let f = [0, 1];
+//     while(f.length - 1 < n) {
+//         f.push(f[f.length - 1] + f[f.length-2]);
+//     }
+//     return f[f.length-1];
+// }
+
+// function fib(n, f = [0, 1]) {
+//     if (f.length - 1 < n) {
+//         f.push(f[f.length - 1] + f[f.length-2]);
+//         fib(n, f);
+//     }
+//     return f[f.length-1];
+// }
+
+
+function fib(n) {
+    if(n < 2) return n;
+    return fib(n-1) + fib(n-2);
+}
 
 module.exports = fib;
